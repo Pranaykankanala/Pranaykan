ed9bf831948e1835131fa64e6ee4ff5d03275474
Pranaykankanala <pranay_kankanala@yahoo.co.in>
2021-04-06 09:41:10 +0000

    string print

--- Programs.js
+++ Programs.js
@@ -1,3 +1,15 @@
 // Print even numbers from 20 to 1
 for (let c=20;c>=1;c-=2)
 console.log(c) 
+
+let data = [10,20,30,40,50,60,70]
+
+for(let i=0;i<data.length;i++){
+    console.log(data[i]);
+}
+
+let username = "sudhakar"
+console.log(username.length);
+for(let i=0;i<username.length;i++){
+    console.log(username[i]);
+}
