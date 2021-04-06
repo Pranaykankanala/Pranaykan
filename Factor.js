4b7e7be7cd25f331f39d66a227028b25de2d372e
Pranaykankanala <pranay_kankanala@yahoo.co.in>
2021-04-06 11:40:54 +0000

    factors

---
+++ Factor.js
@@ -0,0 +1,15 @@
+//find factors of a given number
+n=20;
+for(let i=1;i<=20;i++) {
+    if (n%i==0) {
+        console.log(i);
+    }
+}
+
+//find factorial of a given number
+let n1 = 10;
+let res = 1;
+for(let i=1;i<=n1;i++){
+    res= res*i
+    console.log(res);
+}
