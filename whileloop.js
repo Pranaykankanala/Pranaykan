09271b57b6c61c1a5cdc4c0b7d7a20d0dbf66aec
Pranaykankanala <pranay_kankanala@yahoo.co.in>
2021-04-06 11:10:20 +0000

    while loop

---
+++ whileloop.js
@@ -0,0 +1,15 @@
+let n = 1;
+//while loop execute zero or more times untill condition fail
+//while checks condition and execute block
+
+//do while execute block and checks the condition
+//do while execute one or more times untill condition fail
+
+while (n<10){ //executes until condition fail
+    console.log("execute something");
+    n++;
+}
+do {
+    console.log("execute nothing");
+    n++;
+}while(n<20)
