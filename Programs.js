8e467100911b19c6c8903629a85dd688d4ca2948
Pranaykankanala <pranay_kankanala@yahoo.co.in>
2021-04-06 10:35:41 +0000

    loops

--- Programs.js
+++ Programs.js
@@ -16,7 +16,49 @@ for(let i=0;i<username.length;i++){
 
 let den = [2000,500,200,100,50,20,10,5,2,1];
 let amount = 9999
-let r = amount / den[0];
-console.log(amount % den[0]);
-amount = amount % den[0];
-console.log(amount);
+//let r = parseInt (amount / den[0]);
+//console.log(amount % den[0]);
+//amount = amount % den[0];
+//console.log(amount);
+
+for(let d = 0;d<den.length;d++){
+    let res = parseInt (amount/den[d]);
+    console.log(res+"*"+den[d]);
+    amount = amount % den[d];
+}
+
+// let numberof2k = parseInt(amount/den[0])
+// console.log(numberof2k+"*"+den[0])
+// amount = amount % den[0];
+
+// let numberof5h = parseInt(amount/den[1])
+// console.log(numberof5h+"*"+den[1])
+// amount = amount % den[1];
+
+// let numberof2h = parseInt(amount/den[2])
+// console.log(numberof2h+"*"+den[2])
+// amount = amount % den[2];
+// let numberof1h = parseInt(amount/den[3])
+// console.log(numberof1h+"*"+den[3])
+// amount = amount % den[3];
+// let numberof50 = parseInt(amount/den[4])
+// console.log(numberof50+"*"+den[4])
+// amount = amount % den[4];
+// let numberof20 = parseInt(amount/den[5])
+// console.log(numberof20+"*"+den[5])
+// amount = amount % den[5];
+// let numberof10 = parseInt(amount/den[6])
+// console.log(numberof10+"*"+den[6])
+// amount = amount % den[6];
+// let numberof5 = parseInt(amount/den[7])
+// console.log(numberof5+"*"+den[7])
+// amount = amount % den[7];
+// let numberof2 = parseInt(amount/den[8])
+// console.log(numberof2+"*"+den[8])
+// amount = amount % den[8];
+// let numberof1 = parseInt(amount/den[9])
+// console.log(numberof1+"*"+den[9])
+// amount = amount % den[9];
+
+
+
---
+++ odd number loop.js
@@ -0,0 +1,7 @@
+//find odd numbers between a range
+
+for (let n = 10; n <= 50; n++) {
+    if(n%2 ==1){
+        console.log(n + " is odd number");
+    }
+}
