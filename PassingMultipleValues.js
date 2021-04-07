31daa9f52de2bbe644c6cef6a6f77c93d84e685f
Pranaykankanala <pranay_kankanala@yahoo.co.in>
2021-04-07 10:34:38 +0000

    MultiVal

--- PassingMultipleValues.js
+++ PassingMultipleValues.js
@@ -16,3 +16,14 @@ function demoAddAll(n){
 }
 let arr=[10,20,30,40,50];
 console.log(demoAddAll(arr));
+console.log(demoAddAll([10,20,30,40,50]));
+
+function demoAddAll2(...n) { //rest parameter : accepts any number of values
+     let out = 0;
+     for (let i=0;i<n.length;i++){
+         out=out+n[i];
+     }
+     return out;
+}
+console.log(demoAddAll2(2,5,1,4,6));
+console.log(demoAddAll2(...arr)); //spread parameter
