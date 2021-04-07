a75bf4247ae15d9c2aee1fc734456c35f604d859
Pranaykankanala <pranay_kankanala@yahoo.co.in>
2021-04-07 09:13:32 +0000

    FuncReturn

---
+++ FunctionReturn.js
@@ -0,0 +1,10 @@
+function findFactorial(n){
+    let res =1;
+    for(let i=1;i<=1;i++){
+        res= res*i
+
+    }
+    return res;
+}
+let r = findFactorial(10)
+console.log("The factorial value is"+r);
