ff3494ebbce714ce6c504f25eabfe6776b1d1be1
Pranaykankanala <pranay_kankanala@yahoo.co.in>
2021-04-08 10:03:28 +0000

    JSOb

---
+++ JSObject.js
@@ -0,0 +1,24 @@
+let tool = {
+    version:4,
+    languages:["java","js","python","ruby","c#"],
+    name:"selenium",
+
+    start:function(){
+        console.log(this.name+" is started");
+        },
+        stop:function(){
+            console.log(this.name+" is stopped");
+        }
+}
+console.log(tool["name"]);
+console.log(tool["version"]);
+
+console.log(tool.name);
+console.log(tool.version);
+console.log(tool.languages);
+
+tool.start()
+tool.stop()
+
+//Javascript object notation : JSON
+
